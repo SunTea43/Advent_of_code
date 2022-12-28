ad8625c9bc6a882175436489a259367ecadf6e38
Santiago Pérez <santipego0001@gmail.com>
2022-12-27 21:42:58 -0500

    Solved puzzle 1 part 1 and 2. Added ortography fixes

--- day 1/main.rb
+++ day 1/main.rb
@@ -1,20 +1,20 @@
 file = File.open("input.txt","r")
 lines = file.readlines
 elves=[]
-current_elve = {number: elves.size, calories:0}
-maximum_elve = current_elve
+current_elf = {number: elves.size, calories:0}
+maximum_elf = current_elf
 lines.each_with_index do |line,idx|
     if line.strip!.empty?
-       if current_elve[:calories]>maximum_elve[:calories]
-        maximum_elve=current_elve
+       if current_elf[:calories]>maximum_elf[:calories]
+        maximum_elf=current_elf
        end
-        current_elve = {number: elves.size,calories: 0}
+        current_elf = {number: elves.size,calories: 0}
     else
-        current_elve[:calories] +=line.to_i
+        current_elf[:calories] +=line.to_i
     end
 end
-if current_elve[:calories]>maximum_elve[:calories]
-    maximum_elve=current_elve
+if current_elf[:calories]>maximum_elf[:calories]
+    maximum_elf=current_elf
 end
    
-puts maximum_elve
+puts maximum_elf
---
+++ day 1/part2.rb
@@ -0,0 +1,27 @@
+def update_elves(elves=[],new_elf={})
+    if elves.size<3
+        elves<<new_elf
+    else
+        minimum_elve=elves.min_by{|elf| elf[:calories]}
+        if minimum_elve[:calories]<new_elf[:calories]
+            elves.delete minimum_elve
+            elves<<new_elf
+        end
+    end
+    elves
+end
+file = File.open("input.txt","r")
+lines = file.readlines
+elves =[]
+current_elf = {number: 0, calories:0}
+lines.each_with_index do |line,idx|
+    if line.strip!.empty?
+       elves = update_elves(elves,current_elf) 
+       current_elf = {number: current_elf[:number]+1, calories:0}
+    else
+        current_elf[:calories] +=line.to_i
+    end
+end
+elves = update_elves(elves,current_elf)
+puts elves 
+puts elves.map{|elve| elve[:calories]}.sum

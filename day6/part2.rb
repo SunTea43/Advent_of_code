72a42626b1e2045d45ea1ff996e3b6cdf28bcc08
Santiago Pérez <santipego0001@gmail.com>
2023-01-13 20:34:56 -0500

    Added solution of day 6

---
+++ day6/part2.rb
@@ -0,0 +1,14 @@
+file = File.open("input.txt","r")
+line = file.readline
+characters_to_be_processed =[]
+line.each_char.with_index do |c,idx|
+    characters_to_be_processed<<c
+    if characters_to_be_processed.size==14
+        if characters_to_be_processed.uniq.size == characters_to_be_processed.size
+            puts idx+1
+            break
+        else
+            characters_to_be_processed.delete_at 0
+        end
+    end
+end
